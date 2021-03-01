0008b4cfbc2e6607fb1b4d10fe067210711714ba
fehsand <fehsand@yahoo.com>
2021-03-01 14:47:20 +0300

    Gelistirme.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,9 @@ app_name = "mainapp"
 
 urlpatterns = [
     path('', views.anasayfa, name='anasayfa'),
+    path('hesap/', views.hesap, name='hesap'),
+    path('members/', views.members, name='members'),
+    path('projects/', views.projects, name='projects'),
+    path('products/', views.products, name='products'),
+    path('analysis/', views.analysis, name='analysis'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def anasayfa(request):
-    return render(request, 'mainapp/anasayfa.html', {})
+    items_members = User.objects.all()
+    return render(request, 'mainapp/anasayfa.html', {'members': items_members})
+
+def hesap (request):
+    return render(request, 'mainapp/hesap.html', {})
+
+def members (request):
+    return render(request, 'mainapp/members.html', {})
+
+def projects (request):
+    return render(request, 'mainapp/projects.html', {})
+
+def products (request):
+    return render(request, 'mainapp/products.html', {})
+
+def analysis (request):
+    return render(request, 'mainapp/analysis.html', {})
